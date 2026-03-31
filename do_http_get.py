a789899aa119ac45a4b3989fcb1b247dfaba2e29
Josh Terrill <josh@hacked.codes>
2026-03-30 22:26:11 -0700

    added retry with backoff, added --check-etag to skip already downloaded files, --threads for multi thread processing, added better download logging

--- do_http_get.py
+++ do_http_get.py
@@ -1,11 +1,63 @@
 import requests
+import time
+import sys
 
-def do_get(url, access_token):
+MAX_RETRIES = 5
+INITIAL_BACKOFF = 1  # seconds
+RETRYABLE_STATUS_CODES = {429, 500, 502, 503, 504}
 
-    bearer_headers = {'Content-Type': 'application/json',
-                      'Accept': 'application/json',
-                      'Authorization': 'Bearer {0}'.format(access_token)}
 
-    response = requests.get(url, params=None, headers=bearer_headers, stream=True)
+def _make_request(method, url, access_token, stream=False):
+    bearer_headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer {0}".format(access_token),
+    }
+
+    backoff = INITIAL_BACKOFF
+    last_exception = None
+
+    for attempt in range(MAX_RETRIES):
+        try:
+            if method == "GET":
+                response = requests.get(
+                    url, params=None, headers=bearer_headers, stream=stream
+                )
+            elif method == "HEAD":
+                response = requests.head(url, headers=bearer_headers)
+            else:
+                raise ValueError(f"Unsupported method: {method}")
+
+            if response.status_code not in RETRYABLE_STATUS_CODES:
+                return response
+
+            # Retryable status code
+            if attempt < MAX_RETRIES - 1:
+                sys.stderr.write(
+                    f"Request to {url} returned {response.status_code}, retrying in {backoff}s (attempt {attempt + 1}/{MAX_RETRIES})\n"
+                )
+                time.sleep(backoff)
+                backoff *= 2
+            else:
+                return response
+
+        except requests.exceptions.RequestException as e:
+            last_exception = e
+            if attempt < MAX_RETRIES - 1:
+                sys.stderr.write(
+                    f"Request to {url} failed: {e}, retrying in {backoff}s (attempt {attempt + 1}/{MAX_RETRIES})\n"
+                )
+                time.sleep(backoff)
+                backoff *= 2
+            else:
+                raise last_exception
 
     return response
+
+
+def do_get(url, access_token):
+    return _make_request("GET", url, access_token, stream=True)
+
+
+def do_head(url, access_token):
+    return _make_request("HEAD", url, access_token)
--- download.py
+++ download.py
@@ -2,70 +2,93 @@ import json
 import sys
 import os
 import datetime
+import argparse
+import glob as glob_module
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from email.message import Message
+from rich.progress import (
+    Progress,
+    BarColumn,
+    DownloadColumn,
+    TransferSpeedColumn,
+    TimeRemainingColumn,
+)
+from rich.console import Console
 from do_authentication import authenticate
-from do_http_get import do_get
+from do_http_get import do_get, do_head
+
+console = Console()
+
+parser = argparse.ArgumentParser(description="Download CZDS zone files")
+parser.add_argument(
+    "--threads", type=int, default=1, help="Number of concurrent downloads (default: 1)"
+)
+parser.add_argument(
+    "--check-etag",
+    action="store_true",
+    help="Check ETag before downloading; saves files with ETag in filename and skips if already exists",
+)
+args = parser.parse_args()
 
 ##############################################################################################################
 # First Step: Get the config data from config.json file
 ##############################################################################################################
 
 try:
-    if 'CZDS_CONFIG' in os.environ:
-        config_data = os.environ['CZDS_CONFIG']
+    if "CZDS_CONFIG" in os.environ:
+        config_data = os.environ["CZDS_CONFIG"]
         config = json.loads(config_data)
     else:
-        config_file = open("config.json", "r")
-        config = json.load(config_file)
-        config_file.close()
+        with open("config.json", "r") as config_file:
+            config = json.load(config_file)
 except Exception as e:
     sys.stderr.write("Error loading config.json file: %s\n" % str(e))
     exit(1)
 
-# The config.json file must contain the following data:
-username = config['icann.account.username']
-password = config['icann.account.password']
-authen_base_url = config['authentication.base.url']
-czds_base_url = config['czds.base.url']
-tlds = config.get('tlds', [])
-
-# This is optional. Default to current directory
-working_directory = config.get('working.directory', '.') # Default to current directory
+username = config["icann.account.username"]
+password = config["icann.account.password"]
+authen_base_url = config["authentication.base.url"]
+czds_base_url = config["czds.base.url"]
+tlds = config.get("tlds", [])
+excluded_tlds = config.get("excluded_tlds", [])
+working_directory = config.get("working.directory", ".")
 
 if not username:
-    sys.stderr.write("'icann.account.username' parameter not found in the config.json file\n")
+    sys.stderr.write(
+        "'icann.account.username' parameter not found in the config.json file\n"
+    )
     exit(1)
 
 if not password:
-    sys.stderr.write("'icann.account.password' parameter not found in the config.json file\n")
+    sys.stderr.write(
+        "'icann.account.password' parameter not found in the config.json file\n"
+    )
     exit(1)
 
 if not authen_base_url:
-    sys.stderr.write("'authentication.base.url' parameter not found in the config.json file\n")
+    sys.stderr.write(
+        "'authentication.base.url' parameter not found in the config.json file\n"
+    )
     exit(1)
 
 if not czds_base_url:
     sys.stderr.write("'czds.base.url' parameter not found in the config.json file\n")
     exit(1)
 
-
 ##############################################################################################################
 # Second Step: authenticate the user to get an access_token.
-# Note that the access_token is global for all the REST API calls afterwards
 ##############################################################################################################
 
-print("Authenticate user {0}".format(username))
+console.print(f"Authenticate user {username}")
 access_token = authenticate(username, password, authen_base_url)
-
-
 
 ##############################################################################################################
 # Third Step: Get the download zone file links
 ##############################################################################################################
 
-# Function definition for listing the zone links
+
 def get_zone_links(czds_base_url):
-    global  access_token
+    global access_token
 
     links_url = czds_base_url + "/czds/downloads/links"
     links_response = do_get(links_url, access_token)
@@ -74,90 +97,208 @@ def get_zone_links(czds_base_url):
 
     if status_code == 200:
         zone_links = links_response.json()
-        print("{0}: The number of zone files to be downloaded is {1}".format(datetime.datetime.now(),len(tlds) or len(zone_links)))
+        console.print(
+            f"{datetime.datetime.now()}: The number of zone files to be downloaded is {len(tlds) or len(zone_links)}"
+        )
         return zone_links
     elif status_code == 401:
-        print("The access_token has been expired. Re-authenticate user {0}".format(username))
+        console.print(
+            f"The access_token has been expired. Re-authenticate user {username}"
+        )
         access_token = authenticate(username, password, authen_base_url)
-        get_zone_links(czds_base_url)
+        return get_zone_links(czds_base_url)
     else:
-        sys.stderr.write("Failed to get zone links from {0} with error code {1}\n".format(links_url, status_code))
+        sys.stderr.write(
+            f"Failed to get zone links from {links_url} with error code {status_code}\n"
+        )
         return None
 
 
-# Get the zone links
 zone_links = get_zone_links(czds_base_url)
 if not zone_links:
     exit(1)
-
-
 
 ##############################################################################################################
 # Fourth Step: download zone files
 ##############################################################################################################
 
+
 def _parse_header(header):
     m = Message()
-    m['content-type'] = header
+    m["content-type"] = header
     return m
 
-# Function definition to download one zone file
-def download_one_zone(url, output_directory):
-    print("{0}: Downloading zone file from {1}".format(str(datetime.datetime.now()), url))
 
-    global  access_token
+def _clean_etag(etag):
+    if not etag:
+        return None
+    return etag.strip('"').replace("/", "_").replace("\\", "_")
+
+
+def _get_tld_from_url(url):
+    return url.rsplit("/", 1)[-1].rsplit(".")[-2]
+
+
+def _find_existing_file(output_directory, tld, etag, expected_size=None):
+    pattern = os.path.join(output_directory, f"{tld}_*.txt.gz")
+    existing_files = glob_module.glob(pattern)
+    for filepath in existing_files:
+        filename = os.path.basename(filepath)
+        if f"_{etag}." in filename:
+            if expected_size is not None:
+                actual_size = os.path.getsize(filepath)
+                if actual_size == expected_size:
+                    return filepath
+                # File exists but is incomplete/wrong size
+                return None
+            return filepath
+    return None
+
+
+def download_one_zone(url, output_directory, check_etag=False, progress=None):
+    global access_token
+    tld = _get_tld_from_url(url)
+    etag = None
+
+    # If check_etag is enabled, do a HEAD request first to get ETag
+    if check_etag:
+        head_response = do_head(url, access_token)
+        if head_response.status_code == 200:
+            etag = _clean_etag(head_response.headers.get("ETag"))
+            expected_size = int(head_response.headers.get("Content-Length", 0)) or None
+            if etag:
+                existing_file = _find_existing_file(
+                    output_directory, tld, etag, expected_size
+                )
+                if existing_file:
+                    console.print(
+                        f"{datetime.datetime.now()}: Skipping {tld} - already have complete file with ETag {etag}"
+                    )
+                    return True
+        elif head_response.status_code == 401:
+            console.print(
+                f"The access_token has expired. Re-authenticate user {username}"
+            )
+            access_token = authenticate(username, password, authen_base_url)
+            return download_one_zone(url, output_directory, check_etag, progress)
+
     download_zone_response = do_get(url, access_token)
-
     status_code = download_zone_response.status_code
 
     if status_code == 200:
-        # Try to get the filename from the header
-        option = _parse_header(download_zone_response.headers['content-disposition'])
-        filename = option.get_param('filename')
+        if not etag:
+            etag = _clean_etag(download_zone_response.headers.get("ETag"))
 
-        # If could get a filename from the header, then makeup one like [tld].txt.gz
+        option = _parse_header(download_zone_response.headers["content-disposition"])
+        filename = option.get_param("filename")
+
         if not filename:
-            filename = url.rsplit('/', 1)[-1].rsplit('.')[-2] + '.txt.gz'
+            filename = tld + ".txt.gz"
 
-        # This is where the zone file will be saved
-        path = '{0}/{1}'.format(output_directory, filename)
+        if check_etag and etag:
+            name_part = filename.rsplit(".txt.gz", 1)[0]
+            filename = f"{name_part}_{etag}.txt.gz"
 
-        with open(path, 'wb') as f:
-            for chunk in download_zone_response.iter_content(1024):
-                f.write(chunk)
+        path = f"{output_directory}/{filename}"
+        total_size = int(download_zone_response.headers.get("content-length", 0))
+        chunk_size = 8192
 
-        print("{0}: Completed downloading zone to file {1}".format(str(datetime.datetime.now()), path))
+        if progress:
+            task_id = progress.add_task(filename, total=total_size)
+            with open(path, "wb") as f:
+                for chunk in download_zone_response.iter_content(chunk_size):
+                    f.write(chunk)
+                    progress.update(task_id, advance=len(chunk))
+            progress.remove_task(task_id)
+        else:
+            with Progress(
+                "[progress.description]{task.description}",
+                BarColumn(),
+                "[progress.percentage]{task.percentage:>3.1f}%",
+                DownloadColumn(),
+                TransferSpeedColumn(),
+                TimeRemainingColumn(),
+            ) as single_progress:
+                task_id = single_progress.add_task(filename, total=total_size)
+                with open(path, "wb") as f:
+                    for chunk in download_zone_response.iter_content(chunk_size):
+                        f.write(chunk)
+                        single_progress.update(task_id, advance=len(chunk))
+
+        console.print(
+            f"{datetime.datetime.now()}: Completed downloading zone to file {path}"
+        )
+        return True
 
     elif status_code == 401:
-        print("The access_token has been expired. Re-authenticate user {0}".format(username))
+        console.print(
+            f"The access_token has been expired. Re-authenticate user {username}"
+        )
         access_token = authenticate(username, password, authen_base_url)
-        download_one_zone(url, output_directory)
+        return download_one_zone(url, output_directory, check_etag, progress)
     elif status_code == 404:
-        print("No zone file found for {0}".format(url))
+        console.print(f"No zone file found for {url}")
+        return False
     else:
-        sys.stderr.write('Failed to download zone from {0} with code {1}\n'.format(url, status_code))
+        sys.stderr.write(
+            f"Failed to download zone from {url} with code {status_code}\n"
+        )
+        return False
 
-# Function definition for downloading all the zone files
-def download_zone_files(urls, working_directory):
 
-    # The zone files will be saved in a sub-directory
+def download_zone_files(urls, working_directory, num_threads=1, check_etag=False):
     output_directory = working_directory + "/zonefiles"
 
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    # Download the zone files one by one
+    # Filter URLs based on tlds and excluded_tlds config
+    download_urls = []
     for link in urls:
+        is_excluded = any(link.endswith("%s.zone" % tld) for tld in excluded_tlds)
+        if is_excluded:
+            continue
+
         if len(tlds):
             for tld in tlds:
-                if link.endswith('%s.zone' % tld):
-                    download_one_zone(link, output_directory)
+                if link.endswith("%s.zone" % tld):
+                    download_urls.append(link)
         else:
-            download_one_zone(link, output_directory)
+            download_urls.append(link)
+
+    if num_threads > 1:
+        console.print(f"Downloading with {num_threads} threads...")
+        with Progress(
+            "[progress.description]{task.description}",
+            BarColumn(),
+            "[progress.percentage]{task.percentage:>3.1f}%",
+            DownloadColumn(),
+            TransferSpeedColumn(),
+            TimeRemainingColumn(),
+        ) as progress:
+            with ThreadPoolExecutor(max_workers=num_threads) as executor:
+                futures = {
+                    executor.submit(
+                        download_one_zone, url, output_directory, check_etag, progress
+                    ): url
+                    for url in download_urls
+                }
+                for future in as_completed(futures):
+                    url = futures[future]
+                    try:
+                        future.result()
+                    except Exception as e:
+                        sys.stderr.write(f"Error downloading {url}: {e}\n")
+    else:
+        for link in download_urls:
+            download_one_zone(link, output_directory, check_etag)
+
 
 # Finally, download all zone files
 start_time = datetime.datetime.now()
-download_zone_files(zone_links, working_directory)
+download_zone_files(zone_links, working_directory, args.threads, args.check_etag)
 end_time = datetime.datetime.now()
 
-print("{0}: DONE DONE. Completed downloading all zone files. Time spent: {1}".format(str(end_time), (end_time-start_time)))
+console.print(
+    f"{end_time}: Done. Completed downloading all zone files. Time spent: {end_time - start_time}"
+)
